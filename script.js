506f48973b6b8f921156935ba56a48aca833b89e
Mistermanch <165008101+Mistermanch@users.noreply.github.com>
2026-06-29 19:07:27 +0200

    tired of this library

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ $(function () {
 
     console.log("turn:", $.fn.turn);
 
-    if (!$.fn.turn) {
-        console.error("turn.js NO está cargando");
-        return;
-    }
-
     $(".flipbook").turn({
         width: 922,
         height: 600,
         autoCenter: true,
+        elevation: 50,
+        gradients: true,
         display: "double"
     });
 
