6c5b81555936799e21929c96e84d27d95f4ca66e
Mistermanch <165008101+Mistermanch@users.noreply.github.com>
2026-06-29 18:11:48 +0200

    updated javascript and css

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-$("#flipbook").turn({
-    width: 400,
-    height: 300,
-    autoCenter: true
+function loadApp() {
+
+	var flipbook = $('.flipbook');
+
+ 	// Check if the CSS was already loaded
+	
+	if (flipbook.width()==0 || flipbook.height()==0) {
+		setTimeout(loadApp, 10);
+		return;
+	}
+
+	$('.flipbook .double').scissor();
+
+	// Create the flipbook
+
+	$('.flipbook').turn({
+			// Elevation
+
+			elevation: 50,
+			
+			// Enable gradients
+
+			gradients: true,
+			
+			// Auto center this flipbook
+
+			autoCenter: true
+
+	});
+}
+
+
+
+yepnope({
+	test : Modernizr.csstransforms,
+	yep: ['../../lib/turn.min.js'],
+	nope: ['../../lib/turn.html4.min.js'],
+	both: ['../../lib/scissor.min.js', 'css/double-page.css'],
+	complete: loadApp
 });
