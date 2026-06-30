5911b98dcdfa9e742d2c39c1a0a2c30b4103329f
Mistermanch <165008101+Mistermanch@users.noreply.github.com>
2026-06-30 12:06:13 +0200

    rebuild number something

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-$(".flipbook").turn();
+(function($){
+	$(function(){
+		function initBook(){
+			var w = $(window).width();
+			var h = $(window).height();
+			$('.flipbook').turn({
+				width: w,
+				height: h,
+				autoCenter: true
+			});
+		}
+
+		initBook();
+
+		var resizeTimer;
+		$(window).on('resize', function(){
+			clearTimeout(resizeTimer);
+			resizeTimer = setTimeout(function(){
+				var w = $(window).width();
+				var h = $(window).height();
+				try{
+					$('.flipbook').turn('size', w, h);
+				}catch(e){
+					// if not initialized yet, re-init
+					$('.flipbook').turn('destroy');
+					initBook();
+				}
+			}, 150);
+		});
+	});
+})(jQuery);
